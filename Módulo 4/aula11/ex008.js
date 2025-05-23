f0b3839d02f81cd9be7b99f6ddf9c668e7c8cec4
Daniel Ribeiro Perote <dr853364@gmail.com>
2025-05-23 14:04:06 -0300

    Fiz alguns exercicos incluindo o 08 e 09 iniciando o 010 agora!

---
+++ Módulo 4/aula11/ex008.js
@@ -0,0 +1,6 @@
+var v = 61
+console.log(`A velocidade do seu automovel é ${v}`)
+if (v > 60){ //condição simples
+    console.log("Você ultrapassou a velocidade permitida!")
+}
+console.log("Dirija sempre com cinto de segurança!")
