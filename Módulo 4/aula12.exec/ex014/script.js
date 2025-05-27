6286a239e03baa55b0e6091d716149d383fac6fc
Daniel Ribeiro Perote <dr853364@gmail.com>
2025-05-27 14:37:37 -0300

    finalizei ex014

--- Módulo 4/aula12.exec/ex014/script.js
+++ Módulo 4/aula12.exec/ex014/script.js
@@ -1,1 +1,17 @@
-alert('Olá cabeçudo')
+function carregar() {
+    var texto = window.document.getElementById("texto")
+    var img = document.getElementById('img')
+    var data = new Date()
+    var hora = 19//data.getHours()
+    texto.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 6 && hora < 12 ){
+        img.src = 'img/ft_dia.png'
+        document.body.style.background = '#e4c498'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'img/ft_tarde.png'
+        document.body.style.background = '#DE5201'
+    } else {
+        img.src = 'img/ft_noite.png'
+        document.body.style.background = '#0F222C'
+    }
+}
