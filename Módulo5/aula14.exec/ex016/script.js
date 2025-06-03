7ee09fcc488513dfd64b64e600cba7faf6633bfc
Daniel Ribeiro Perote <dr853364@gmail.com>
2025-06-03 18:32:22 -0300

    mega comit de exercicios

---
+++ Módulo5/aula14.exec/ex016/script.js
@@ -0,0 +1,27 @@
+function calculo() {
+    let ini = document.getElementById('ini')
+    let fim = document.getElementById('fim')
+    let pas = document.getElementById('pas')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.lengt == 0){
+        alert('Erro! faltam dados.')
+    }else{
+        res.innerHTML= ("Contando:")
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        if (i < f) {
+            for(let c = 1; c <=f; c +=p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
+            
+    
---
+++ Módulo6/aula015/ambiente.js
@@ -0,0 +1,5 @@
+let a = [1,6,2,8,3,5,2]
+//console.log(a)
+for(let pos in a){
+    console.log(a[pos])
+}
