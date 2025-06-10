7bcade30316f54162309b71839392c305c14f638
Daniel Ribeiro Perote <dr853364@gmail.com>
2025-06-10 15:22:06 -0300

    a

--- Módulo5/aula13/node_72d7eb88ea3b4.tmp.js
+++ Módulo5/aula13/node_72d7eb88ea3b4.tmp.js
@@ -1,12 +1,26 @@
-var c = 1
-do {
-    console.log('Olá')
-    
-} while (c <=6)
+ var pastel = ['queijo', 'presunto', 'carne', 'frango']
+ var tal = 1
+ var tel
+ switch (tal) {
+    case 0:
+        tel = pastel[0]
+        console.log('pastel de queijo');
+        break
+    case 1:
+        tel = pastel[1]
+        console.log('pastel de presunto');
+        break
+    case 2:
+        tel = pastel [2]
+        console.log(`pastel de ${pastel[2]}`);
+        break
+    case 3:
+        tel = pastel[3]
+        console.log(`pastel de ${pastel[3]}`) 
+        break
+        default:
+            console.log('Erro tal');
+            break          
+ }
 
-/* var c = 1
-while(c <= 6){
-    console.log('Olá')
-    c++
-}
-*/
+    
